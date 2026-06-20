286be46bca9e0ce6362c2b474ce0983cb49ab063
disservin <disservin.social@gmail.com>
2026-06-20 17:57:46 +0200

    feat: automatically use _pdep when available (#22)
    Uses runtime dispatch logic to use _pdep when the CPU has bmi2 and the function isn't microcoded. 
    
    ```
    auto-detect-bmi2:
    
    Processed:    133925670 entries |     17 392 939 entries/sec | elapsed:      7.7s
    
    
    main:
    
    Processed:     92453586 entries |     14 007 260 entries/sec | elapsed:      6.6s
    
    
    main+bmi2:
    
    Processed:    121619157 entries |     17 374 062 entries/sec | elapsed:      7.0s
    
    ```

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -4,4 +4,5 @@ pub mod compressed_move;
 pub mod compressed_position;
 pub mod compressed_training_file_reader;
 pub mod compressed_training_file_writer;
+pub(crate) mod cpu_features;
 pub mod entry;
